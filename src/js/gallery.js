29be7b755ced23b82d62178b8ff189b14626f797
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-11-28 23:19:03 -0800

    created folder structure

---
+++ src/js/gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+/**
+ * 
+ * const imgContainer = document.querySelector('.gallery');
+
+images.forEach(image => {
+  const imgElement = `<li class="gallery-item">
+    <a class="gallery-link" href="${image.original}">
+      <img
+        class="gallery-image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </li>`;
+  imgContainer.innerHTML += imgElement;
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+  scrollZoom: false,
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    lightbox.close();
+  }
+});
+ */
